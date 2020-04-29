29c8f5d87d8d47bbe4078c67e1fccabd93cd9f1f
Eduardo Lemos <didonet@gmail.com>
2020-04-29 08:07:28 -0700

    mudando path chromedriver

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -44,7 +44,7 @@ module.exports = {
                 waitForConditionTimeout: defaultTimeout 
             },
             webdriver: {
-                server_path: "node_modules/chromedriver/lib/chromedriver",
+                server_path: chromedriver.path,
                 port: 9515
             },
             desiredCapabilities: {
